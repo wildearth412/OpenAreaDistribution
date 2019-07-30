032c0ee80d92b0f01a9a1782fe1d5a64cea82e79
oukenshu <oukenshu@team-lab.com>
2019-07-30 19:06:41 +0900

    Add assets.

---
+++ OpenAreaDistribution/Assets/_OpenAreaDistribution/Scripts/PositionSetting.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionSetting : MonoBehaviour
+{
+    public int boundPixelOffset = 10;
+    public LineRenderer lineNorth;
+    public LineRenderer lineSouth;
+    public LineRenderer lineWest;
+    public LineRenderer lineEast;
+
+    public GameObject prefab;
+    public GameObject prefab2;
+    public int minPointsNum = 20;
+    public int maxPointsNum = 30;
+
+    private Vector3 pos1, pos2, pos3, pos4;
+    private List<GameObject> points = new List<GameObject>();
+
+	void Start ()
+    {
+        var pos0 = Camera.main.WorldToScreenPoint(Vector3.zero);
+        pos1 = Camera.main.ScreenToWorldPoint(new Vector3(boundPixelOffset,Camera.main.pixelHeight - boundPixelOffset, pos0.z));
+        pos2 = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.pixelWidth - boundPixelOffset, Camera.main.pixelHeight - boundPixelOffset, pos0.z));
+        pos3 = Camera.main.ScreenToWorldPoint(new Vector3(boundPixelOffset, boundPixelOffset, pos0.z));
+        pos4 = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.pixelWidth - boundPixelOffset, boundPixelOffset, pos0.z));
+        lineNorth.SetPositions(new Vector3[] { pos1, pos2});
+        lineSouth.SetPositions(new Vector3[] { pos3, pos4});
+        lineWest.SetPositions(new Vector3[] { pos1, pos3 });
+        lineEast.SetPositions(new Vector3[] { pos2, pos4 });
+
+        var num = Random.Range(minPointsNum,maxPointsNum);
+        for(int i = 0; i < num; i++)
+        {
+            CreatePoint();
+        }
+	}
+	
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.Space))
+        {
+            int n = points.Count - 1;
+            for (int i = n; i >= 0; i--)
+            {
+                Destroy(points[i]);
+            }
+            points.Clear();
+            var num = Random.Range(minPointsNum, maxPointsNum);
+            for (int i = 0; i < num; i++)
+            {
+                CreatePoint();
+            }
+        }
+	}
+
+    void CreatePoint()
+    {
+        var posX = Random.Range(pos1.x, pos2.x);
+        var posY = Random.Range(pos3.y, pos1.y);
+        var obj = Instantiate(prefab,new Vector3(posX,posY,0),Quaternion.identity);
+        points.Add(obj);
+    }
+
+    
+}
---
+++ OpenAreaDistribution/Assets/_OpenAreaDistribution/Scripts/RandomColor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomColor : MonoBehaviour
+{
+
+	void Start ()
+    {
+        var rand = Random.Range(0.05f,0.15f);
+        transform.localScale = new Vector3(rand, rand, rand);
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        var rand2 = Random.Range(0.15f, 0.37f);
+        sr.color = new Color(rand2, rand2, rand2);
+	}
+
+}
